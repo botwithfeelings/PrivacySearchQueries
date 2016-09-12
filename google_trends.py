76c6a64acf5a59ff669d0dc27c8b1a3a21fb2de1
shaowns <shaown@csebuet.org>
2016-09-12 05:15:30 -0400

    Fixed broken pytrends

--- google_trends.py
+++ google_trends.py
@@ -3,15 +3,17 @@
 
 # In[1]:
 
-from pytrends.pyGTrends import pyGTrends
+from pytrends.request import TrendReq
+from random import randint
 import csv
 import argparse
 import traceback
+import pandas as pd
 
 
-# In[4]:
+# In[7]:
 
-def get_trend_data(conn, term, path, label):
+def get_trend_data(t, term, label):
     # Concoct the dictionary for querying gtrends.
     payload = dict()
     payload['q'] = term
@@ -19,8 +21,9 @@ def get_trend_data(conn, term, path, label):
     payload['hl'] = 'en-US'
     payload['date'] = '01/2011 66m'
     try:
-        conn.request_report(payload)
-        conn.save_csv(path, label)
+        df = t.trend(payload, return_type='dataframe')
+        filename = './gtrends/' + label + '.csv'
+        df.to_csv(filename)
     except Exception as e:
         print str(e)
         print traceback.print_exc()
@@ -33,17 +36,17 @@ def main():
     ap.add_argument('-f', '-file', help='CSV file containing trend keywords at column 0', required=True)
     args = ap.parse_args()
     
-    conn = pyGTrends('privacyprojectncsu1@gmail.com','privacyproject')
+    pyTrends = TrendReq('privacyprojectncsu1@gmail.com','privacyproject')
     with open(args.f, 'r') as f:
         reader = csv.reader(f)
         for row in reader:
             trend_term = row[0]
             trend_label = trend_term.replace('/', '_')
-            get_trend_data(conn, trend_term, './gtrends/', trend_label)
+            get_trend_data(pyTrends, trend_term, trend_label)
     return
 
 
-# In[5]:
+# In[ ]:
 
 if __name__ == '__main__':
     main()
