66abc8e1adc9b5fcade1d05c1418d568a4e6c9b9
ssarker <ssarker@ncsu.edu>
2016-11-21 05:09:43 -0500

    Adding google related search

---
+++ google_query_similarity.py
@@ -0,0 +1,182 @@
+
+# coding: utf-8
+
+# In[2]:
+
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.porter import PorterStemmer
+from bs4 import BeautifulSoup
+from random import randint
+from time import sleep
+from collections import OrderedDict
+import numpy as np
+import argparse
+import csv
+import urllib
+import urllib2
+import unicodedata
+import string
+import nltk
+import sys
+
+
+# In[3]:
+
+"""All lexical representations are to be used is the stemmed representation by the Porter stemmer"""
+stemmer = PorterStemmer()
+
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+
+
+# In[4]:
+
+"""Tokenizes and performs stemming on the tokens."""
+def tokenize(text):
+    text = text.lower()
+    tokens = nltk.word_tokenize(text)
+    stems = stem_tokens(tokens, stemmer)
+    return stems
+
+
+# In[5]:
+
+"""Convert unicode to ascii, removes accents."""
+def to_ascii(s):
+    return unicodedata.normalize('NFKD', s).encode('ascii', 'ignore')
+
+
+# In[6]:
+
+"""Prepares for centroid calculation:
+- Calculate L2 norm for each row
+- Normalize the row by L2 norm"""
+def tfidf_normalize(row):
+    # Calculate L2 norm value.
+    l2norm = np.linalg.norm(row, 2)
+
+    # Normalize row by L2 norm.
+    return row/l2norm
+
+
+# In[7]:
+
+"""Takes in the sparse matrix"""
+def get_query_expansion_vector(vec):
+    # Calculate the vector.
+    normalized = np.apply_along_axis(tfidf_normalize, axis=1, arr=vec)
+    centroid = np.sum(normalized, axis=0)
+    qe = centroid/np.linalg.norm(centroid, 2)
+    return qe
+
+
+# In[8]:
+
+"""Retrieves html result for a query pushed into the Google search engine."""
+def get_query_html(query):
+    try:
+        address = "http://www.google.com/search?q=%s&num=100&hl=en&start=0" % (urllib.quote_plus(query))
+        request = urllib2.Request(address, None, {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'})
+        urlfile = urllib2.urlopen(request)
+        page = urlfile.read()
+        return page
+    except Exception as e:
+        print 'Error retrieving google search results: ' + str(e)
+        print 'Google throttling, wait a couple of minutes and try again.'
+        sys.exit()
+
+
+# In[9]:
+
+"""Retrives the tf-idf matrix for a query and candidate."""
+def get_tfidf_matrices(es_q, es_c):
+    lq = len(es_q)
+    lc = len(es_c)
+
+    # Combine the document sets for tf-idf calculation.
+    combined = es_q
+    combined.extend(es_c)
+
+    # We want to get a new vectorizer for every string.
+    tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
+    tfs = tfidf.fit_transform(combined)
+
+    vectors = tfs.toarray()
+    return vectors[0:lq-1, :], vectors[lq:lq+lc-1, :]
+
+
+# In[10]:
+
+"""Calculates the kernel function value from two expansion sets rather
+than raw short string candidates, makes caching easier."""
+def kval_es(es_q, es_c):
+    vq, vc = get_tfidf_matrices(es_q, es_c)
+    qe_q = get_query_expansion_vector(vq)
+    qe_c = get_query_expansion_vector(vc)
+    return np.inner(qe_q, qe_c)
+
+
+# In[13]:
+
+"""Finds the value of the kernel function between
+query string q and candidate c, betther not use this,
+cause this is costly. Use the extended set version when
+handling bulk. Use this for checking or debugging."""
+def kval(q, c):
+    q_page = get_query_html(q)
+    c_page = get_query_html(c)
+    es_q = google_expanded_docs(q_page)
+    es_c = google_expanded_docs(c_page)
+    return kval_es(es_q, es_c)
+
+
+# In[12]:
+
+"""Retrieves the related searches for a Google query string,
+given the html page of the google search page."""
+def google_related_searches(page):
+    rs = list()
+    if page is not None:
+        soup = BeautifulSoup(page, 'lxml')
+        # Strip the related search portion using beautifulsoup.
+        rsdiv = soup.find("div", { "id" : "brs" })
+        for d in rsdiv.findAll('div', {'class':'brs_col'}):
+            for p in d.findAll('p', {'class':'_e4b'}):
+                rs.append(to_ascii(p.getText()).translate(None, string.punctuation))
+
+    return rs
+
+"""Retrieves the first 100 expanded sets for a query string,
+given the html page of the google search page.Expanded set
+consists of the result header and the summary text
+strip for each of the search results in the returned page."""
+def google_expanded_docs(page):
+    es = list()
+    if page is not None:
+        soup = BeautifulSoup(page, 'lxml')
+        # Strip the extended set using beautifulsoup.
+        esdivs = soup.findAll("div", { "class" : "g" })
+        for esdiv in esdivs:
+            for d in esdiv.findAll('div', {'class':'rc'}):
+                doc = ""
+                for hr in d.findAll('h3', {'class':'r'}):
+                    doc += hr.getText()
+                    doc += ' '
+                for ds in d.findAll('div', {'class':'s'}):
+                    for s in ds.findAll('span', {'class' : 'st'}):
+                        doc += s.getText()
+                es.append(to_ascii(doc).translate(None, string.punctuation))
+
+    return es
+
+
+# In[14]:
+
+def test_kval(q, c):
+    k = kval(q, c)
+    print k
--- google_related_queries.py
+++ google_related_queries.py
@@ -1,196 +1,112 @@
 
 # coding: utf-8
 
-# In[17]:
+# In[10]:
 
-from nltk.corpus import stopwords
-from nltk import word_tokenize
+import google_query_similarity as gr
 from collections import OrderedDict
-from random import shuffle
-from bs4 import BeautifulSoup
-from time import sleep
 import argparse
 import csv
-import urllib
-import urllib2
 
 
 # In[ ]:
 
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
-
-# In[15]:
-
-"""A method that cleans the list of obtained queries by removing stopwords
-from every query and return the cleaned queries as a list"""
-def clean_obtained_queries(queries, stop):
-    clean_queries = []
-    for q in queries:
-        words = word_tokenize(q)
-        query = (" ".join([i for i in words if i not in stop])).strip(' .,\'')
-        clean_queries.append(query)
-    return clean_queries
-
-
-# In[13]:
-
-"""A method that calculates overlap between a list of related 
-queries and the global set of already approved queries."""
-def get_overlap(approved_queries, approved_ngrams, related_queries, method):
-    if method == 'n' and len(approved_ngrams) == 0:
-        return 1    
-    if method == 's' or len(approved_queries) == 0:
-        return 1    
-    if len(related_queries) == 0:
-        return 0
-    count = 0.0
-    print approved_ngrams
-    print related_queries
-    for item in related_queries:
-        if method == 'n':
-            item_set = set(item.split())
-            if len(item_set) in approved_ngrams:
-                len_list = approved_ngrams[len(item_set)]
-                if len_list is not None:
-                    for ngram in len_list:
-                        inter = item_set & set(ngram.split())
-                        if len(inter) == len(item_set):
-                            count += 1
-                            break
-        elif method == 's':
-            if item in approved_queries.keys():
-                count += 1
-            
-    control_size = min(len(approved_queries), len(related_queries))
-    return count/control_size
-
-
-# In[14]:
-
-def save_related_queries(seed, threshold, factor, method, approved, rejected):
-    name_suffix = './googledata/' + seed.replace(' ', '_') + '_' + str(threshold) + '_' + str(factor) + '_' + method + '_'
+def save_related_queries(seed, approved, rejected):
+    name_suffix = './googledata/' + seed.replace(' ', '_') + '_' 
     a_name =  name_suffix + 'approved.csv'
     r_name = name_suffix + 'rejected.csv'
         
     with open(a_name, 'w') as f:
         csv_writer = csv.writer(f, lineterminator='\n')
-        for (k, v) in approved.iteritems():
+        for (k, v) in approved_queries.iteritems():
             csv_writer.writerow([k, v])
             
     with open(r_name, 'w') as f:
         csv_writer = csv.writer(f, lineterminator='\n')
-        for (k, v) in rejected.iteritems():
+        for (k, v) in rejected_queries.iteritems():
             csv_writer.writerow([k, v])    
 
 
-# In[18]:
+# In[11]:
 
-def google_related_searches(query):
-    try:
-        address = "http://www.google.com/search?q=%s&num=100&hl=en&start=0" % (urllib.quote_plus(query))
-        request = urllib2.Request(address, None, {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'})
-        urlfile = urllib2.urlopen(request)
-        page = urlfile.read()
-    except Exception as e:
-        print 'Error retrieving google search results: ' + str(e)
-        return False, list()
+def do_stuff(seed, limit):
+    # Expansion set and first iteration of  for the seed.
+    seed_page = gr.get_query_html(seed)
+    seed_rs = gr.google_related_searches(seed_page)
+    seed_es = gr.google_expanded_docs(seed_page)
     
-    # Strip the related search portion using beautifulsoup.
-    rs = list()
-    soup = BeautifulSoup(page, 'lxml')
-    rsdiv = soup.find("div", { "id" : "brs" })
-    for d in rsdiv.findAll('div', {'class':'brs_col'}):
-        for p in d.findAll('p', {'class':'_e4b'}):
-            rs.append(p.getText())
-    return True, rs
+    # Dictionaries of approved and rejected queries along with their kernel value.
+    approved = OrderedDict()
+    rejected = OrderedDict()
+    
+    # We do not want to go beyond a certain iteration.
+    iteration = 0
+    candidates = list()
+    
+    # Initiate the candidates with the seed's related queries.
+    candidates.extend(seed_rs)
+    while iteration < limit:
+        next_candidates = list()
+        threshold = 1.0
+        while len(candidates) > 0:
+            candidate = candidates.pop(0)
+            # If the candidate appears in either the accepted
+            # or rejected sets then don't process it.
+            if (candidate in approved) or (candidate in rejected):
+                continue
+            
+            # Get the candidate's related searches and extended set.
+            can_page = gr.get_query_html(candidate)
+            can_rs = gr.google_related_searches(can_page)
+            can_es = gr.google_expanded_docs(can_page)
+            
+            # Retrieve the kernel value.
+            kval = gr.kval_es(seed_es, can_es)
+            
+            # If this is the first iteration accept all.
+            # Otherwise if the kernel value is less than
+            # the threshold then reject it.
+            if iteration == 0:
+                # For the first iteration accept everything.
+                approved[candidate] = kval
+                
+                # Set the threshold to the minimum of the first iteration.
+                if kval < threshold:
+                    threshold = kval                
+            else:
+                # For further iteration, only accept if kernel 
+                # value is greater than the threshold.
+                if kval > threshold:
+                    approved[candidate] = kval 
+                else:
+                    rejected[candidate] = kval                
+            
+            # Add the candidate's related searches to the next_candidates.
+            next_candidates.extend(can_rs)
+            
+        # Add the next candidates to the candidates set
+        # and increase the iteration count.
+        candidates.extend(next_candidates)
+        iteration += 1
+        
+    save_related_queries(seed, approved, rejected)
+    return
 
 
-# In[ ]:
+# In[8]:
 
 def main():
-    # Structure to hold search queries by the length of their ngram. This is
-    # find a faster lookup for determining overlap values quickly.
-    approved_ngrams = dict()
-
-    #A dictionary of approved queries along with their overlap value
-    approved_queries = OrderedDict() 
-    rejected_queries = OrderedDict()
-
-    # A set maintained for related queries where the overlap value was zero.
-    # This can happen when either none of the related keywords appear in the
-    # approved ngrams or we could not find any related keywords for the
-    # given seed query.
-    junk_related_keywords = set()
-    
-    stop = stopwords.words('english')
-    
-    ap = argparse.ArgumentParser(description='Find related searches using the google search scraping.')
+    ap = argparse.ArgumentParser(description='Use the script to pull google related search queries.')
     ap.add_argument('-s', '-seed', help='Seed word', required=True)
-    ap.add_argument('-t', '-threshold', help='Overlapping threshold', default=0.25)
-    ap.add_argument('-f', '-factor', help='Selection factor of approved related keywords', default=1.00)
-    ap.add_argument('-o', '-overlap', help='Overlapping measurement method (n)gram or (s)tring', default='n')
+    ap.add_argument('-i', '-iteration', help='limit to number of related search iteration', default=3)
+    
     args = ap.parse_args()
     
-    # Default values
-    seed_word = args.s
-    threshold = float(args.t)
-    selection_factor = float(args.f)
-    overlap_method = args.o
+    seed = args.s
+    limit = args.i
     
-    try:
-        newset = [seed_word]
-        while 0 < len(newset) and len(newset) < 40000:
-            new_query = newset.pop(0)
-
-            # If this query appears anywhere in the approved, rejected, or
-            # junk queries, then don't process it.
-            if  (new_query in approved_queries) or (new_query in rejected_queries) or (new_query in junk_related_keywords):
-                continue
-
-            # Fetch the related searches.
-            success, related_keywords = google_related_searches(new_query)            
-            
-            # Check if we failed to retrieve the related searches.
-            if not success:
-                # Put back the query in the candidate set and try again.
-                newset.insert(0, new_query)
-                continue
-
-            # Remove stopwords from the related keywords.
-            related_keywords = clean_obtained_queries(related_keywords, stop)
-
-            # Get overlap value
-            overlap_value = get_overlap(approved_queries, approved_ngrams, related_keywords, overlap_method)
-
-            if overlap_value >= threshold:
-                # Overlap value is beyond threshold. Save the new 
-                # query's overlap value and add it to the approved
-                # queries list.
-                approved_queries[new_query] = overlap_value
-
-                # The related keywords are now cadidates for further
-                # related keyword fetching.
-                subset_index = int(selection_factor * len(related_keywords))
-                shuffle(related_keywords)
-                newset.extend(related_keywords[0:subset_index])
-
-                # Add the new query to the approved ngrams.
-                l = len(new_query.split())
-                approved_ngrams.setdefault(l, list()).append(new_query)
-            else:
-                # Reject the new query.
-                rejected_queries[new_query] = overlap_value
-
-                # If the overlap value is zero, then add them to junk queries.
-                if overlap_value == 0:
-                    junk_related_keywords |= set(related_keywords)
-    except Exception as e:
-        print 'Error processing related search ' + str(e)
-    finally:
-        save_related_queries(seed_word, threshold, selection_factor, overlap_method, approved_queries, rejected_queries)
+    do_stuff(seed, limit)
+    return
 
 
 # In[ ]:
