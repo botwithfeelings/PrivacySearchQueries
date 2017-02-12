a25e94be9695c2fa8448010f601c5c6473c0d285
ssarker <ssarker@ncsu.edu>
2017-02-12 01:16:00 -0500

    saving

--- pulltrends.sh
+++ pulltrends.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+# Usage ./pulltrends.sh 200 (cmp)
 cnt=$1
+cmp=$2
 filename="seed_queries.txt"
 while read -r line
 do
     SEED=$line
     echo "Current Seed: $SEED"
     SEED_FILE="./googledata/${SEED// /_}_approved.csv"
-    python google_trends.py -f "$SEED_FILE" -s "$SEED" -k $cnt >> log.txt
+    if ["$cmp" == "cmp"]; then
+      python google_trends.py -f "$SEED_FILE" -s "$SEED" -k $cnt -cmp >> log.txt
+    else
+      python google_trends.py -f "$SEED_FILE" -s "$SEED" -k $cnt >> log.txt
+    fi
 done < "$filename"
