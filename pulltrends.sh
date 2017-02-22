fac5112ae71fad6fde259dcedd29ed9aea86a3b5
ssarker <ssarker@ncsu.edu>
2017-02-22 17:54:24 -0500

    saving

--- pulltrends.sh
+++ pulltrends.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-# Usage ./pulltrends.sh 200 (cmp)
+# Usage ./pulltrends.sh 200 (cmp|scale)
 cnt=$1
-cmp=$2
+cs=$2
 filename="seed_queries.txt"
 while read -r line
 do
     SEED=$line
     echo "Current Seed: $SEED"
     SEED_FILE="./googledata/${SEED// /_}_approved.csv"
-    if [ "$cmp" == "cmp" ]; then
+    if [ "$cs" == "scale" ]; then
+      python google_trends.py -s "$SEED" -k $cnt -scale >> log.txt
+    elif [ "$cs" == "cmp" ]; then
       python google_trends.py -f "$SEED_FILE" -s "$SEED" -k $cnt -cmp >> log.txt
     else
       python google_trends.py -f "$SEED_FILE" -s "$SEED" -k $cnt >> log.txt
