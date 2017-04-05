1558ae34c8103ffb9164b8c1e96a7e2df98e35d5
ssarker <ssarker@ncsu.edu>
2017-04-05 16:14:09 -0400

    saving

--- pullrelatedqueries.sh
+++ pullrelatedqueries.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Usage ./pullrelatedqueries.sh 150 (noamt|amt)
+# Usage ./pullrelatedqueries.sh 150 (noamt|amt) (nocomp|comp)
 cnt=$1
 amt=$2
+comp=$3
 if [ "$amt" == "amt" ]; then
   filename="seed_queries_amt.txt"
 elif [ "$amt" == "noamt" ]; then
@@ -12,5 +13,10 @@ while read -r line
 do
     SEED=$line
     echo "Current Seed: $SEED"
-    python google_related_queries.py -s "$SEED" -k $cnt >> pull_related_queries_log.txt
+    if [ "$comp" == "comp" ]; then
+      SEED_FILE="./surveyQueries/grouped_by_seed/${SEED// /_}_approved.csv"
+      python google_related_queries.py -s "$SEED" -k $cnt -f $SEED_FILE -comp >> amt_index_recall.csv
+    elif [ "$comp" == "nocomp" ]; then
+      python google_related_queries.py -s "$SEED" -k $cnt >> pull_related_queries_log.txt
+    fi    
 done < "$filename"
